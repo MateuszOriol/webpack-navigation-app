7568c96785a477b078e1a2c2c5815a048046c179
MateuszOriol <s26499@pjwstk.edu.pl>
2023-12-23 00:10:00 +0100

    ss

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,18 @@
-const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
-
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-
+  // ... other configurations
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
-  },
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
+
